ebfd98809dc72db677f9694b84658ba157cff3a5
DJPC <davidmarkjones86@gmail.com>
2023-01-23 14:12:24 +0000

    building blocks of storage function

--- starter/app.js
+++ starter/app.js
@@ -14,28 +14,41 @@ The following animation demonstrates the application functionality: */
 let saveBtn = $('.saveBtn')
 let textEls = $('.description')
 console.log(textEls);
-let currentDay = '14'/*moment().format('dddd, MMMM Do YYYY');*/
+let currentDay = moment().format('dddd, MMMM Do YYYY')
 $('#currentDay').text(currentDay);
 
 // change bootstrap class depending upon time past present future//
 let currentHour = moment().format('kk');
 
-
-for (const element of saveBtn) {
-    element.addEventListener('click', function() {
-        console.log('works');
-    });
+function storageFunc () {
+    let value = localStorage.getItem('userInput')
+    
+    localStorage.setItem('userInput', myEl);
+    console.log('storageFunc')
+    
 }
-
-for (const element of textEls) {
-    let timeNum = Number(element.id)
-    console.log(timeNum)
-    if(currentDay > timeNum ) {
-        element.classList.add('past');
-        console.log('class added')
-    } else if (currentDay < timeNum) {
-        element.classList.add('future')  
-    } else {
-        element.classList.add('present');
+function eventListeners() {
+for (const element of saveBtn) {
+    element.addEventListener('click', storageFunc());
     }
 }
+
+function pageLogic() {
+    for (const element of textEls) {
+        myEl = element.textContent;
+        console.log(`myEl ${myEl}`)
+        let timeNum = Number(element.id)
+        console.log(timeNum)
+        if(currentHour > timeNum ) {
+            element.classList.add('past');
+            console.log('class added')
+        } else if (currentHour < timeNum) {
+            element.classList.add('future')  
+        } else {
+            element.classList.add('present');
+        }
+    }
+    return myEl;
+}
+
+window.eventListeners('load', pageLogic())
